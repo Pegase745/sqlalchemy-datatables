aad87d7bca5237272e61ee0912dc5e06c8733b4d
Pegase745 <michel.nemnom@gmail.com>
2013-08-07 23:24:15 +0200

    Initial commit

---
+++ datatables/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+__VERSION__ = '0.0.0'
+
+from datatables import *
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+__VERSION__ = [line for line in file('datatables/__init__.py', 'rb') \
+               if line.startswith('__VERSION__')][0].split(\
+               '=')[1].strip().lstrip('\'').rstrip('\'')
+
+setup(
+    name='sqlalchemy-datatables',
+    version=__VERSION__,
+    description='SQLAlchemy integration of jQuery DataTables',
+    long_description=open('README.md').read(),
+    license='MIT',
+    keywords='sqlalchemy datatables jquery pyramid',
+    author='Michel Nemnom',
+    author_email='michel.nemnom+pypi@gmail.com',
+    url='https://github.com/pegase745/sqlalchemy-datatables',
+    packages=find_packages(exclude=['test_app']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['sqlalchemy'],
+    py_modules=['datatables'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
