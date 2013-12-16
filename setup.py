5a4d898cb0c4d151a7dd46b1bc944f22a1e4b69a
Pegase745 <michel.nemnom@gmail.com>
2013-12-16 11:08:28 +0100

    Allowing to run with python 3.*

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-__VERSION__ = [line for line in file('datatables/__init__.py', 'rb') \
-               if line.startswith('__VERSION__')][0].split(\
-               '=')[1].strip().lstrip('\'').rstrip('\'')
+import sys
+
+if sys.version_info<= (2,7):
+    f = open('datatables/__init__.py', 'rb')
+else:
+    f = open('datatables/__init__.py', 'rb', encoding="utf-8")
+
+
+__VERSION__ = [line for line in f \
+    if line.startswith('__VERSION__')][0].split(\
+    '=')[1].strip().lstrip('\'').rstrip('\'')
+
 
 setup(
     name='sqlalchemy-datatables',
@@ -21,14 +30,25 @@ setup(
     install_requires=['sqlalchemy'],
     py_modules=['datatables'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Pyramid',
+        'Framework :: Flask',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
