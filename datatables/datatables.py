04334d7896e3df622474200a7f69aed7ccd0c680
Damian Dimmich <damian@tauri-tec.com>
2014-11-12 01:48:42 +0400

    add ability to override searchability serverside (prevent clients from doing things they shouldn't)
    Add ability to search user.address.country correctly.

--- datatables/datatables.py
+++ datatables/datatables.py
@@ -2,6 +2,7 @@
 from sqlalchemy.sql.expression import asc, desc
 from sqlalchemy.sql import or_, and_
 from sqlalchemy.orm.properties import RelationshipProperty
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm.collections import InstrumentedList
 from sqlalchemy.sql.expression import cast
 from sqlalchemy import String
@@ -11,7 +12,7 @@ from logging import getLogger
 
 log = getLogger(__file__)
 
-ColumnTuple = namedtuple('ColumnDT', ['column_name', 'mData', 'search_like', 'filter'])
+ColumnTuple = namedtuple('ColumnDT', ['column_name', 'mData', 'search_like', 'filter', 'searchable'])
 
 
 def get_attr(sqla_object, attribute):
@@ -38,15 +39,17 @@ class ColumnDT(ColumnTuple):
     :param filter: the method needed to be executed on the cell values of the column 
     as an equivalent of a jinja2 filter (default None)
     :type filter: a callable object
+    :param searchable: Enable or disable a column to be searchable server-side. (default True)
+    :type searchable: bool
 
     :returns: a ColumnDT object 
     """
-    def __new__(cls, column_name, mData=None, search_like=None, filter=str):
+    def __new__(cls, column_name, mData=None, search_like=None, filter=str, searchable=True):
         """
         On creation, sets default None values for mData and string value for
         filter (cause: Object representation is not JSON serializable)
         """
-        return super(ColumnDT, cls).__new__(cls, column_name, mData, search_like, filter)
+        return super(ColumnDT, cls).__new__(cls, column_name, mData, search_like, filter, searchable)
 
 
 class DataTables:
@@ -133,29 +136,39 @@ class DataTables:
         search_value = self.request_values.get('sSearch')
         condition = None
         def search(idx, col):
+            #TODO: fix for @hybrid properties that reference json or similar columns.
             tmp_column_name = col.column_name.split('.')
-            obj = getattr(self.sqla_object, tmp_column_name[0])
-            if not hasattr(obj, "property"): # Ex: hybrid_property or property
-                sqla_obj = self.sqla_object
-                column_name = col.column_name
-            elif isinstance(obj.property, RelationshipProperty): #Ex: ForeignKey
-                # Ex: address.description
-                sqla_obj = obj.mapper.class_
-                column_name = "".join(tmp_column_name[1:])
-                if not column_name:
-                    # find first primary key
-                    column_name = obj.property.table.primary_key.columns \
-                        .values()[0].name
-            else:
-                sqla_obj = self.sqla_object
-                column_name = col.column_name
+            for tmp_name in tmp_column_name:
+                #This handles the x.y.z.a option
+                if tmp_column_name.index(tmp_name) == 0:
+                    obj = getattr(self.sqla_object, tmp_name)
+                    parent = self.sqla_object
+                elif isinstance(obj.property, RelationshipProperty):
+                    #otherwise try and see if we can percolate down the list for relationships of relationships.
+                    parent = obj.property.mapper.class_
+                    obj = getattr(parent, tmp_name)
+
+                if not hasattr(obj, 'property'): # Ex: hybrid_property or property
+                    sqla_obj = parent
+                    column_name = tmp_name
+                elif isinstance(obj.property, RelationshipProperty): #Ex: ForeignKey
+                    # Ex: address.description
+                    sqla_obj = obj.mapper.class_
+                    column_name = tmp_name
+                    if not column_name:
+                        # find first primary key
+                        column_name = obj.property.table.primary_key.columns \
+                            .values()[0].name
+                else:
+                    sqla_obj = parent
+                    column_name = tmp_name
             return sqla_obj, column_name
 
         if search_value:
             conditions = []
             for idx, col in enumerate(self.columns):
                 if self.request_values.get('bSearchable_%s' % idx) in (
-                        True, 'true'):
+                        True, 'true') and col.searchable:
                     sqla_obj, column_name = search(idx, col)
                     conditions.append(cast(get_attr(sqla_obj, column_name), String).ilike('%%%s%%' % search_value))
             condition = or_(*conditions)
