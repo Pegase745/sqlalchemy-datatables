b2c3df9a73d7945f613af2a23f4a75420d4c74d9
Pegase745 <michel.nemnom@gmail.com>
2015-04-13 17:00:16 +0200

    Hotfix imports

--- datatables/__init__.py
+++ datatables/__init__.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+import sys
+
+if sys.version_info>(3,0):
+    from datatables.datatables import ColumnDT, DataTables
+else:
+    from datatables import ColumnDT, DataTables
+
+
 __VERSION__ = '0.1.7'
 
-from datatables.datatables import ColumnDT, DataTables
