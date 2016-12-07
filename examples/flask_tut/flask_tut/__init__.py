4713fb714903dc34bb7a9d48dcb167fc7ad74905
Goran Rakic <grakic@devbase.net>
2016-12-07 11:06:50 +0100

    Update Flask example. (#75)
    * Update Flask example to new API
    
    * Drop support for datatables 1.9

--- examples/flask_tut/flask_tut/__init__.py
+++ examples/flask_tut/flask_tut/__init__.py
@@ -21,12 +21,6 @@ def home():
     return render_template('home.html', project='flask_tut')
 
 
-@app.route('/dt_19x', methods=['GET'])
-def dt_19x():
-    """List users with DataTables <= 1.9.x."""
-    return render_template('dt_19x.html', project='dt_19x')
-
-
 @app.route('/dt_110x', methods=['GET'])
 def dt_110x():
     """List users with DataTables <= 1.10.x."""
@@ -37,17 +31,21 @@ def dt_110x():
 def data():
     """Return server side data."""
     # defining columns
-    columns = []
-    columns.append(ColumnDT('id'))
-    columns.append(ColumnDT('name', filter=upper))
-    columns.append(ColumnDT('address.description'))
-    columns.append(ColumnDT('created_at'))
+    columns = [
+        ColumnDT(User.id),
+        ColumnDT(User.name),
+        ColumnDT(Address.description),
+        ColumnDT(User.created_at)
+    ]
 
     # defining the initial query depending on your purpose
-    query = db.session.query(User).join(Address).filter(Address.id > 14)
+    query = db.session.query().select_from(User).join(Address).filter(Address.id > 14)
+
+    # GET parameters
+    params = request.args.to_dict()
 
     # instantiating a DataTable for the query and table needed
-    rowTable = DataTables(request.args, User, query, columns)
+    rowTable = DataTables(params, query, columns)
 
     # returns what is needed by DataTable
     return jsonify(rowTable.output_result())
